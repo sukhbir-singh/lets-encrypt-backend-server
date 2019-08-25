44653558cd0346c08fe1639c735de64ea48a8120
Aditya Singh <linkadityasingh@gmail.com>
2019-08-25 15:18:24 +0530

    generate cert working via backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "/dashboard" => "lets_encrypt#dashboard"
-  post "/certi/status" => "lets_encrypt#check_status"
-  post "/certi/generate" => "lets_encrypt#generate_certificate"
+  get "/certi/status" => "lets_encrypt#check_status"
+  get "/certi/generate" => "lets_encrypt#generate_certificate"
   post "/certi/adduser" => "lets_encrypt#add_user"
   post "/certi/renew" => "lets_encrypt#renew_certificate"
   post "/certi/delete" => "lets_encrypt#delete_certificate"
--- lib/helper_methods.rb
+++ lib/helper_methods.rb
@@ -4,32 +4,33 @@ require 'net/http'
 class HelperMethods
     def self.generateCert(params)
         # get variables
-        api = params[:api_key]
-        @user = AmahiUser.where("api_key = ?", api)
-        @hdaname = @user.pluck(:hdaname).first
-        @amahi_user_id = @user.pluck(:id).first
+        
 
         cert_status = statusCert(params)
-        return (cert_status)
+        if cert_status == "Not Available"
+            initiateCertGen(params)
+        else
+            return (cert_status)
+        end
 
 
         # fetch hda name from db
 
 
         # change domain name to yourhda.com
-        @domain_name = 'linksam.tk'
+        #@domain_name = 'linksam.tk'
 
         #check if processing or already generated
         #if LetsEncryptLog.exist?(@amahi_user_id)
 
 
         # insert into db with status pending
-        status = LetsEncryptLog.add(:amahi_users_id => @amahi_user_id, :status => "Processing")
-        if status.save
-            render :json => {success: true, last_generated_at: "16 september 2019 12:00:01", expiry_date: "8 december 2019 11:59:00"}
-        else
-        	render :json => {success: false, last_generated_at: "16 september 2019 12:00:01", expiry_date: "8 december 2019 11:59:00"}
-        end
+        #status = LetsEncryptLog.add(:amahi_users_id => @amahi_user_id, :status => "Processing")
+        #if status.save
+        #    render :json => {success: true, last_generated_at: "16 september 2019 12:00:01", expiry_date: "8 december 2019 11:59:00"}
+        #else
+        #	render :json => {success: false, last_generated_at: "16 september 2019 12:00:01", expiry_date: "8 december 2019 11:59:00"}
+        #end
         
         # request cert factory to generate
 
@@ -43,7 +44,7 @@ class HelperMethods
 
         #request.body = {"hda_name" => "#{@user}", "domain" => "#{@domain_name}"}.to_json # SOME JSON DATA e.g {msg: 'Why'}.to_json
 
-        return (status)
+        #return (status)
 
 
         #response = http.request(request)
@@ -65,6 +66,33 @@ class HelperMethods
 
         # forward the cert to plugin for install
         #
+    end
+
+    def self.initiateCertGen(params)
+        api = params[:api_key]
+        @user = AmahiUser.where("api_key = ?", api)
+        @hdaname = @user.pluck(:hdaname).first
+        @amahi_user_id = @user.pluck(:id).first
+
+        # change domain name to yourhda.com
+        @domain_name = 'linksam.tk'
+
+        # insert into db with status pending
+        status = LetsEncryptLog.new(:amahi_users_id => @amahi_user_id, :status => "Processing")
+        status.save
+
+        # request cert factory to generate
+
+        # provide url of cert factory server
+        url = "http://localhost:5004/api/v1/certi/generate"
+        uri = URI(url)
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = false   # make it true in production
+        request = Net::HTTP::Post.new(uri.path, {'Content-Type' => 'application/json'})
+        request.body = {"hda_name" => "#{@hdaname}", "domain" => "#{@domain_name}"}.to_json # SOME JSON DATA e.g {msg: 'Why'}.to_json
+        http.request(request)
+
+        return (status)
     end
 
     def self.revokeCert(params)
@@ -89,7 +117,14 @@ class HelperMethods
 
         if LetsEncryptLog.exists?(@amahi_user_id)
             current_status = LetsEncryptLog.where("amahi_users_id = ?", @amahi_user_id).pluck(:status).first
-            return (current_status)
+            if current_status == "Processing"
+                #check status of cert in cert factory
+                # if not generated
+                return ("Under Process. Please wait.")
+                #generated change status to issued and get the certificate
+            elsif current_status == "Issued"
+                return ("Already issued")
+            end          
         else
             current_status = "Not Available"
             return (current_status)
